1be8e9a09ab1afe32dfd197aa1e25b7b4e51ab4e
Scott Bradley <scottbradley.mail@gmail.com>
2020-10-25 10:55:45 -0500

    autorefresh in client

--- client.py
+++ client.py
@@ -17,10 +17,21 @@ client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 token_url = 'http://localhost:8000/token'
 
+
+
+
+
 client = BackendApplicationClient(client_id=client_id)
 oauth = OAuth2Session(client=client)
 token = oauth.fetch_token(token_url=token_url, client_id=client_id, client_secret=client_secret, include_client_id=True)
 print(token)
+
+
+refresh_url = 'http://localhost:8000/token-refresh'
+extra = {}
+
+client = OAuth2Session(client_id, token=token, auto_refresh_url=refresh_url,
+    auto_refresh_kwargs=extra, token_updater=token_saver)
 
 import time
 
@@ -28,14 +39,19 @@ import time
 print('waiting for expire')
 time.sleep(35)
 
-from oauthlib.oauth2 import TokenExpiredError
-
-try:
-    client = OAuth2Session(client_id, token=token)
-    r = client.get('http://localhost:8000/widget')
-except TokenExpiredError as e:
-    token = client.refresh_token('http://localhost:8000/token-refresh')
-    print(token)
-client = OAuth2Session(client_id, token=token)
-r = client.get('http://localhost:8000/widget')
+protected_url = 'http://localhost:8000/widget'
+r = client.get(protected_url)
 print(r)
+
+
+#from oauthlib.oauth2 import TokenExpiredError
+#
+#try:
+#    client = OAuth2Session(client_id, token=token)
+#    r = client.get('http://localhost:8000/widget')
+#except TokenExpiredError as e:
+#    token = client.refresh_token('http://localhost:8000/token-refresh')
+#    print(token)
+#client = OAuth2Session(client_id, token=token)
+#r = client.get('http://localhost:8000/widget')
+#print(r)
