72f37bdefdc9377ae249dc026e3b693d7f66a024
apps <apps@stg-app4.socialcontext.ai>
2020-11-27 22:36:07 +0000

    32 bit client secret

--- app/orm/__init__.py
+++ app/orm/__init__.py
@@ -5,7 +5,7 @@ For details on use of the secrets module for cryptographic key generation see:
 https://docs.python.org/3/library/secrets.html
 """
 OAUTH2_CLIENT_ID_BYTES = 32
-OAUTH2_CLIENT_SECRET_BYTES = 64
+OAUTH2_CLIENT_SECRET_BYTES = 32
 OAUTH2_ACCESS_TOKEN_BYTES = 32
 OAUTH2_REFRESH_TOKEN_BYTES = 64
 
