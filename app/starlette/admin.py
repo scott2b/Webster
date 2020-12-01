230fda100c413a28f8a894e4a4f719d8014b546d
scott2b <scottbradley.mail@gmail.com>
2020-11-30 18:34:44 -0600

    admin scope required for admin page

--- app/starlette/admin.py
+++ app/starlette/admin.py
@@ -1,4 +1,7 @@
+from starlette.authentication import requires
 from .templates import render
 
+
+@requires('admin_auth', status_code=403)
 async def admin(request):
     return render('admin.html', {})
